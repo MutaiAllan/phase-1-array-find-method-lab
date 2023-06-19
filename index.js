3f324c7a06c024ff65a2e9e71c5aaf81ec75bb2b
MutaiAllan <MutaiAllan>
2023-06-19 22:45:02 +0300

    Added file for assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+function superbowlWin(array) {
+    for (key in array) {
+      let won = key.find(isWin => isWin.result == 'W');
+      if (won) {
+        return key.year;
+      } else {
+        return undefined;
+      }
+    }
+}
+// console.log(superbowlWin(record));
